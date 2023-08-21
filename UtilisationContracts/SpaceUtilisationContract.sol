4cd9ad75e0804d82f0a5af252d40a7cd52445d00
dimydemin <post.ddemin@gmail.com>
2023-08-21 14:36:15 +0200

    Update SpaceUtilisationContract.sol

--- UtilisationContracts/SpaceUtilisationContract.sol
+++ UtilisationContracts/SpaceUtilisationContract.sol
@@ -1,22 +1,23 @@
 // SPDX-License-Identifier: MIT
 /*
-In this example, the smart contract defines structures for Bitcoin transactions and room utilizations. 
-The recordUtilization function records room utilization data, and the recordBitcoinTransaction function 
-records Bitcoin transactions. You would need to modify and expand this contract to handle the actual 
-linkage between Bitcoin transactions and room utilization data using appropriate identifiers.
+This smart contract defines structures for Bitcoin transactions and space utilisations in this example. 
+The recordUtilisation function records space utilisation data, and the recordBitcoinTransaction function 
+records Bitcoin transactions. You need to modify and expand this contract using appropriate identifiers to handle the 
+linkage between Bitcoin transactions and space utilisation data.
 
-To deploy and interact with this contract, you can use the Remix IDE or other Ethereum development tools. 
+You can use the Remix IDE or other Ethereum development tools to deploy and interact with this contract. 
 You would deploy the contract to the Ethereum blockchain, and then you could call the functions to record 
-Bitcoin transactions and room utilizations.
+Bitcoin transactions and space utilisations.
 
-Keep in mind that this example is very basic and lacks many real-world considerations, such as security, 
-data validation, access controls, and more. Additionally, handling Bitcoin transactions on Ethereum 
+Remember that this example is basic and lacks many real-world considerations, such as security, 
+data validation, access controls, etc. Additionally, handling Bitcoin transactions on Ethereum 
 requires integration with external data sources or oracles to verify Bitcoin transactions on the Bitcoin network.
 
 */
+
 pragma solidity ^0.8.0;
 
-contract RoomUtilizationContract {
+contract SpaceUtilisationContract {
     struct BitcoinTransaction {
         address sender;
         address receiver;
@@ -24,21 +25,22 @@ contract RoomUtilizationContract {
         uint256 timestamp;
     }
 
-    struct RoomUtilization {
-        uint256 roomId;
-        uint256 utilizationPercentage;
+    struct SpaceUtilisation {
+        uint256 spaceId;
+        uint256 utilisationPercentage;
         string timeIntervals;
+        uint256 area; // field for area
     }
 
-    mapping(bytes32 => RoomUtilization) public roomUtilizations;
+    mapping(bytes32 => SpaceUtilisation) public spaceUtilisations;
 
-    function recordUtilization(uint256 roomId, uint256 utilizationPercentage, string memory timeIntervals) public {
-        bytes32 utilizationHash = keccak256(abi.encodePacked(roomId, utilizationPercentage, timeIntervals));
-        roomUtilizations[utilizationHash] = RoomUtilization(roomId, utilizationPercentage, timeIntervals);
+    function recordUtilisation(uint256 spaceId, uint256 utilisationPercentage, string memory timeIntervals, uint256 area) public {
+        bytes32 utilisationHash = keccak256(abi.encodePacked(spaceId, utilisationPercentage, timeIntervals, area));
+        spaceUtilisations[utilisationHash] = SpaceUtilisation(spaceId, utilisationPercentage, timeIntervals, area);
     }
 
     function recordBitcoinTransaction(address sender, address receiver, uint256 amount, uint256 timestamp) public {
         bytes32 transactionHash = keccak256(abi.encodePacked(sender, receiver, amount, timestamp));
-        // In a real application, you might link the Bitcoin transaction hash to a utilization hash here.
+        // In a real application, you might link the Bitcoin transaction hash to a utilisation hash here.
     }
 }
